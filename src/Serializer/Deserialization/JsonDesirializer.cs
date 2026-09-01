089e64825d3a4deb87a6084f27f31ccc5edd35fb
ARTISNT <ag1545959@gmail.com>
2026-09-01 19:24:40 +0300

    feat: add collection desirialization
    -Add collection desirialization(DesirializeCollection)
    -Add recursioned desirialization
    -Refactor and cleanup
    -Fix some bugs with class desirialization

--- src/Serializer/Deserialization/JsonDesirializer.cs
+++ src/Serializer/Deserialization/JsonDesirializer.cs
@@ -5,19 +5,41 @@ namespace Serializer.Deserialization;
 
 public class JsonDesirializer
 {
-    public static T Desiriailize<T>(string json)
+    public static T? Desiriailize<T>(string json)
     {
         var jsonReader = new JsonReader(json);
-        object outputObject = Activator.CreateInstance<T>()
+
+        jsonReader.Read();
+
+        return (T)DeserializeValue(typeof(T), jsonReader);
+    }
+
+    public static object? Desiriailize(Type targetType, string json)
+    {
+        var jsonReader = new JsonReader(json);
+
+        jsonReader.Read();
+
+        return DeserializeValue(targetType, jsonReader);
+    }
+
+    private static object? DesiriailizeObject(JsonReader reader, Type targetType)
+    {
+        object outputObject = Activator.CreateInstance(targetType)
             ?? throw new NullReferenceException();
 
         PropertyInfo? property = null;
-        while (jsonReader.Read())
+        object? convertedValue;
+
+        while (reader.Read())
         {
-            if (jsonReader.TokenType == JsonToken.PropertyName)
+            if (reader.TokenType is JsonToken.EndObject)
+                return outputObject;
+
+            if (reader.TokenType is JsonToken.PropertyName)
             {
-                property = typeof(T).GetProperty(jsonReader.Value)
-                    ?? throw new FormatException($"Property {jsonReader.Value!} not found");
+                property = targetType.GetProperty(reader.Value!)
+                    ?? throw new FormatException($"Property {reader.Value!} not found");
 
                 continue;
             }
@@ -25,21 +47,60 @@ public class JsonDesirializer
             if (property is null)
                 continue;
 
-            if (jsonReader.TokenType is JsonToken.StartObject or JsonToken.EndObject)
-                continue;
-
-            var convertedValue = ConvertValue(jsonReader, property.PropertyType);
-
+            convertedValue = DeserializeValue(property.PropertyType, reader);
             property.SetValue(outputObject, convertedValue);
         }
 
-        return (T)outputObject;
+        return outputObject;
+    }
+
+    private static object? DesirializeCollection(JsonReader reader, Type targetType)
+    {
+        Type elementType = targetType.IsArray
+            ? targetType.GetElementType()
+            : targetType.GetGenericArguments()[0];
+
+        Type listType = typeof(List<>).MakeGenericType(elementType);
+
+        var list = Activator.CreateInstance(listType);
+
+        var methodAdd = listType.GetMethod("Add");
+
+        while (reader.Read())
+        {
+            if (reader.TokenType is JsonToken.EndArray)
+                break;
+
+            methodAdd!.Invoke(list, [DeserializeValue(elementType, reader)]);
+        }
+
+        if (targetType.IsArray)
+        {
+            return listType.GetMethod("ToArray")!.Invoke(list, null)!;
+        }
+
+        return list;
     }
 
     private static object? ConvertValue(
         JsonReader reader,
         Type targetType)
     {
+        if (targetType.IsEnum)
+        {
+            return reader.TokenType switch
+            {
+                JsonToken.String => Enum.Parse(
+                    targetType, reader.Value!, true),
+
+                JsonToken.Number => Enum.ToObject(targetType, Convert.ToInt32(reader.Value,
+                    CultureInfo.InvariantCulture)),
+
+                _ => throw new FormatException(
+                    $"Cannot convert token {reader.TokenType} to {targetType.Name}.")
+            };
+        }
+
         return reader.TokenType switch
         {
             JsonToken.String =>
@@ -51,13 +112,41 @@ public class JsonDesirializer
                     targetType,
                     CultureInfo.InvariantCulture),
 
-            JsonToken.True => true,
-            JsonToken.False => false,
+            JsonToken.True or JsonToken.False
+                when targetType == typeof(bool)
+                    => reader.TokenType == JsonToken.True,
 
-            JsonToken.Null => null,
+            JsonToken.Null when
+                !targetType.IsValueType ||
+                Nullable.GetUnderlyingType(targetType) != null
+                    => null,
 
             _ => throw new FormatException(
                 $"Cannot convert token {reader.TokenType} to {targetType.Name}.")
         };
     }
+
+    private static object? DeserializeValue(Type targetType, JsonReader reader)
+    {
+        return reader.TokenType switch
+        {
+            JsonToken.StartObject =>
+                DesiriailizeObject(reader, targetType),
+
+            JsonToken.StartArray =>
+                DesirializeCollection(reader, targetType),
+
+            JsonToken.String or
+                  JsonToken.Number or
+                  JsonToken.True or
+                  JsonToken.False or
+                  JsonToken.Null =>
+                      ConvertValue(reader, targetType),
+
+            _ => throw new FormatException(
+                $"Unexpected token {reader.TokenType}.")
+        };
+    }
+
+
 }
